fff86c0281f7c0f842d7f9be66ffbd4d6538e34c
Luca Lawson <luca@lawson.me>
2019-05-09 09:41:56 -0700

    Printing Commands

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -1,7 +1,8 @@
 
 let buttona;
 let buttonb;
-
+let truthset = ["say this","say that","say the other thing"];
+let dareset = ["do this","do that","do the other thing"];
 
 
 
@@ -9,22 +10,29 @@ function setup() {
   createCanvas(1300, 650);
   background(177, 207, 239);
   buttona = createButton('Truth');
-  buttona.position(400, 100);
+  buttona.position(400, 50);
   buttona.mousePressed(changeA);
   buttonb = createButton('Dare');
-  buttonb.position(900, 100);
+  buttonb.position(900, 50);
   buttonb.mousePressed(changeB);
 }
 
 
 function draw(){
-
+  textSize(32);
+  text('or', 650, 65);
+  fill(0, 102, 153);
+  line(0, 100, 1300, 100);
 }
 
 function changeA(){
-
+  var rand = truthset[Math.floor(Math.random() * truthset.length)];
+  textSize(32);
+  text(rand, 50, 325);
 }
 
 function changeB(){
-
+ var rand = dareset[Math.floor(Math.random() * dareset.length)];
+ textSize(32);
+ text(rand, 1000, 325);
 }
