1bcc3d4196a0eeab2525c671ab193c356c7c8218
Danielle <daniellerachelpark@gmail.com>
2019-05-13 14:48:52 -0700

    4 lines

--- 2-array-of-objects/sketch.js
+++ 2-array-of-objects/sketch.js
@@ -1,46 +1,26 @@
 
-//create an empty array called balls
-let balls = [];
+//create a variable to hold one ball
 
 function setup() {
-  createCanvas(800, 400);
+  createCanvas(1300, 650);
 
 }
+
 
 function draw(){
-	background(220);
-
-//	draw all the balls in that array
-	for (let i = 0; i < balls.length; i++) {
-	    balls[i].drawBall();
-        balls[i].moveBall();
-	  }
-}
-
-function keyPressed(){ //every time you push a key, make a new ball from the ball class and add it to the balls array
-  let  b = new Ball(100, 100);
-  balls.push(b);
-  console.log(balls);
-}
-
-//ball class from which to create new balls with similar properties.
-class Ball {
-
-	constructor(x,y){ //every ball needs an x value and a y value
-		    this.x = x;
-    		this.y = y;
-	}
-
-	drawBall(){  // draw a ball on the screen at x,y
-    		stroke(0);
-    		fill("red");
-		    ellipse(this.x,this.y,10,10);
-	}
-
-	moveBall(){ //update the location of the ball, so it moves across the screen
-		this.x = this.x+2;
-		this.y = this.y+.5;
-	}
+ellipse(650,325,350,350);
+fill(247, 111, 147)
 
 
+
+	translate(650,325);
+
+	// then rotate the grid around the pivot point by a
+	// number of degrees equal to the frame count of the sketch
+  rotate(radians(frameCount));
+
+  line(0,-175,0,175);
+  line(-175,0,175,0);
+  line(123.744,123.744,-123.744,-123.7443);
+  line(-123.744,123.744,123.744,-123.744);
 }
