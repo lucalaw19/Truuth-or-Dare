537e319fa9364a37739c771eef864a04c15fdd28
Luca Lawson <luca@lawson.me>
2019-05-17 11:11:14 -0700

    spinnn

--- 2-array-of-objects/sketch.js
+++ 2-array-of-objects/sketch.js
@@ -4,7 +4,9 @@ let buttona;
 let buttonb;
 let truthset = ["say this","say that","say the other thing"];
 let dareset = ["do this","do that","do the other thing"];
-let spinning = false
+let spinning = false;
+let spin_start = 0;
+let rotate_position = 0;
 
 
 function setup() {
@@ -24,64 +26,66 @@ function draw(){
   textSize(32);
   text('or', 650, 65);
   fill(247, 111, 147);
+  ellipse(650,325,350,350);
   line(0, 100, 1300, 100);
-
+  translate(650,325);
+  fill(0);
+  triangle(0, -160, -10, -185, 10, -185);
 
 
 
   // spin();
-
+  push();
   if (spinning == true){
+    if (frameCount <= 200+frame_start){
+      rotate_position= rotate_position +1;
+    }
 
-    ellipse(650,325,350,350);
-    push();
-    print("runns")
-    translate(650,325);
-    
-
-
-  // then rotate the grid around the pivot point by a
-  // number of degrees equal to the frame count of the sketch
-    rotate(radians(frameCount));
-    line(0,-175,0,175);
-    line(-175,0,175,0);
-    line(123.744,123.744,-123.744,-123.7443);
-    line(-123.744,123.744,123.744,-123.744);
-    pop();
   }
+  rotate(radians(rotate_position));
+  line(0,-175,0,175);
+  line(-175,0,175,0);
+  line(123.744,123.744,-123.744,-123.7443);
+  line(-123.744,123.744,123.744,-123.744);
+  pop();
+
 }
 
 function changeA(){
   spinning = true
-  newTruth();
-  fill(0);
-  var rand = truthset[Math.floor(Math.random() * truthset.length)];
-  textSize(32);
-  text(rand, 50, 325);
-  stroke();
+  frame_start = frameCount;
+  drawTruth(truthset[Math.floor(Math.random() * truthset.length)]);
+
 }
 
 function changeB(){
  spinning = true
+ frame_start = frameCount;
  newDare();
  fill(0);
  var rand = dareset[Math.floor(Math.random() * dareset.length)];
  textSize(32);
- text(rand, 1000, 325);
+ text(rand, 300, 0);
  stroke();
 }
 
 
 
-function newTruth(){
+function drawTruth(random){
   noStroke();
   fill(177, 207, 239);
-  rect(10,275,350,75);
-
+  rect(-600,-50,350,75);
+  rect(300,-50,350,75);
+  fill(0);
+  var rand = truthset[Math.floor(Math.random() * truthset.length)];
+  textSize(32);
+  text(random, -600, 0);
+  stroke();
 }
 
 function newDare(){
   noStroke();
   fill(177, 207, 239);
-  rect(900,275,350,75);
+  rect(300,-50,350,75);
+  rect(-600,-50,350,75);
 }
